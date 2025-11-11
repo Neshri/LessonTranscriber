91aa60c7989f7d23ed825f133969cf3527f9374f
Anton Lundgren <anton.lundgren94@gmail.com>
2025-11-11 20:19:19 +0100

    Debugger

--- main.py
+++ main.py
@@ -15,10 +15,9 @@ from database import init_db, insert_processed_file, insert_transcript
 from modules.config import load_config
 from modules.file_utils import get_audio_paths, load_processed_files, get_file_hash, is_file_processed
 from modules.lesson_transcriber import LessonTranscriber
-
 # Configure logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
